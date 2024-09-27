158590f8c7532b8f08b178e371f0e45a3f2f007b
XXXYTKA <167037176+XXXYTKA@users.noreply.github.com>
2024-09-27 20:55:52 +0300

    Add files via upload

---
+++ module_1_5.py
@@ -0,0 +1,11 @@
+immutable_var = (1, 'poison', 3)
+print(immutable_var)
+  #immutable_var[1] = 10          кортеж - неизменяемый структур данных
+mutable_list = [3, 'die', 5]
+print(mutable_list)
+mutable_list[0] = 10
+mutable_list[1] = 'death'
+print(mutable_list)
+
+
+
